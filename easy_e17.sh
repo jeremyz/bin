2cc33ffbcfae1e355245715c830e0a8aadeb8191
Jérémy Zurcher <jeremy@asynk.ch>
2012-01-06 14:14:38 +0100

    easy_e17.sh: remove itask-ng (moved to engage)

--- easy_e17.sh
+++ easy_e17.sh
@@ -48,7 +48,7 @@ bin_basic="exchange e"
 e_modules_bin="emprint exalt"
 e_modules_extra="alarm calendar comp-scale cpu deskshow diskio drawer eenvader.fractal empris engage eooorg e-tiling \
 everything-aspell everything-mpris everything-pidgin everything-places everything-shotgun everything-skeleton everything-tracker everything-wallpaper everything-websearch \
-eweather exalt-client exebuf execwatch flame forecasts iiirk itask itask-ng language mail mem moon mpdule net news penguins photo places quickaccess \
+eweather exalt-client exebuf execwatch flame forecasts iiirk itask language mail mem moon mpdule net news penguins photo places quickaccess \
 rain screenshot skel slideshow snow taskbar tclock tiling uptime weather winlist-ng winselector wlan xkbswitch"
 bin_extra="calculator clouseau converter e_phys editje edje_viewer elfe elmdentica elsa emote empower enjoy enki envision \
     ephoto espionnage Eterm eve excessive expedite exquisite eyelight phonebook rage sticky-notes"
