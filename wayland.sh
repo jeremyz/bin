dc8b98556f8752f0496ec71d43a4ef52a58e66f6
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-05 16:42:51 +0100

    wayland* update

--- wayland.sh
+++ wayland.sh
@@ -10,9 +10,9 @@ C_INCLUDE_PATH=$WLD/include
 LIBRARY_PATH=$WLD/lib
 PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
 
-export WLD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL C_INCLUDE_PATH LIBRARY_PATH PKG_CONFIG_ALLOW_SYSTEM_CFLAGS
+#export WLD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL C_INCLUDE_PATH LIBRARY_PATH PKG_CONFIG_ALLOW_SYSTEM_CFLAGS
 
-BUILD_DIR=${BUILD_DIR:-~/usr/git/wayland}
+BUILD_DIR=${BUILD_DIR:-~/local}
 FORCE_AUTOGEN=0
 FORCE_DISTCLEAN=0
 SUDO_PASSWD=""
@@ -45,8 +45,6 @@ else
 fi
 echo
 
-[ ! -d "$WLD/share/aclocal" ] && echo "$SUDO_PASSWD" | sudo -S mkdir -p "$WLD/share/aclocal"
-
 function build () {
     if [ $FORCE_AUTOGEN -eq 1 -o ! -e Makefile ]; then
         say " * autogen --prefix=$WLD $my_configure_opts" && ./autogen.sh --prefix=$WLD $my_configure_opts
@@ -63,7 +61,7 @@ function update () {
     say " * pull" && git pull || return 1
     SHA_HEAD=$(git log --pretty="format:%H" HEAD~1..)
     if [ $FORCE_AUTOGEN -eq 1 -o $FORCE_DISTCLEAN -eq 1 ]; then
-        autogen
+        build
     else
         [ "$SHA_PREV" = "$SHA_HEAD" ] && return 0
         build
@@ -86,84 +84,6 @@ say "wayland"
 my_dir=wayland
 my_src=git://anongit.freedesktop.org/wayland/wayland
 my_configure_opts=
-do_your_job
-
-say "xcb:pthread-stubs"
-my_dir=pthread-stubs
-my_src=git://anongit.freedesktop.org/xcb/pthread-stubs
-my_configure_opts=
-do_your_job
-
-say "xorg:util:macros"
-my_dir=macros
-my_src=git://anongit.freedesktop.org/xorg/util/macros
-my_configure_opts=
-do_your_job
-
-#say "xorg:util:macros"
-#my_dir=macros
-#my_src=git://anongit.freedesktop.org/git/xorg/util/macros
-#my_configure_opts=
-#do_your_job
-
-say "xorg:proto:xproto"
-my_dir=xproto
-my_src=git://anongit.freedesktop.org/xorg/proto/xproto
-my_configure_opts=
-do_your_job
-
-say "xorg:proto:kbproto"
-my_dir=kbproto
-my_src=git://anongit.freedesktop.org/xorg/proto/kbproto
-my_configure_opts=
-do_your_job
-
-say "xorg:proto:glproto"
-my_dir=glproto
-my_src=git://anongit.freedesktop.org/xorg/proto/glproto
-my_configure_opts=
-do_your_job
-
-say "xorg:proto:dri2proto"
-my_dir=dri2proto
-my_src=git://anongit.freedesktop.org/xorg/proto/dri2proto
-my_configure_opts=
-do_your_job
-
-say "mesa:drm"
-my_dir=drm
-my_src=git://anongit.freedesktop.org/git/mesa/drm
-my_configure_opts="--enable-nouveau-experimental-api"
-do_your_job
-
-say "mesa:mesa"
-my_dir=mesa
-my_src=git://anongit.freedesktop.org/mesa/mesa
-my_configure_opts="--enable-gles2 --disable-gallium-egl --with-egl-platforms=x11,wayland,drm --enable-gbm --enable-shared-glapi"
-do_your_job
-
-say "xorg:lib:libX11"
-my_dir=libX11
-my_src=git://anongit.freedesktop.org/xorg/lib/libX11
-my_configure_opts="--enable-specs=false"
-do_your_job
-
-say "xorg:lib:libxkbcommon"
-my_dir=libxkbcommon
-my_src=git://people.freedesktop.org/xorg/lib/libxkbcommon.git
-my_configure_opts="--with-xkb-config-root=/usr/share/X11/xkb --enable-specs=false"
-do_your_job
-
-say "xorg:pixman"
-my_dir=pixman
-my_src=git://anongit.freedesktop.org/pixman
-my_configure_opts=
-do_your_job
-
-say "xorg:cairo"
-my_dir=cairo
-my_src=git://anongit.freedesktop.org/cairo
-my_configure_opts="--enable-gl --enable-xcb"
 do_your_job
 
 # WAYLAND-DEMOS
