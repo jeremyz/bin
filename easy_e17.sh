e1f3ea504b324e1c38c4c7f7777b8a880deaec3f
Jérémy Zurcher <jeremy@asynk.ch>
2012-06-27 08:25:18 +0200

    easy_e17.sh: replace e_phys with ephysics

--- easy_e17.sh
+++ easy_e17.sh
@@ -57,7 +57,7 @@ e_modules_extra="alarm calendar comp-scale cpu deskshow diskio drawer eenvader.f
 everything-aspell everything-mpris everything-pidgin everything-places everything-shotgun everything-skeleton everything-tracker everything-wallpaper everything-websearch \
 eweather exalt-client exebuf execwatch flame forecasts iiirk mail mem moon mpdule net news penguins photo places quickaccess \
 rain screenshot skel slideshow snow taskbar tclock tiling uptime weather winselector wlan"
-bin_extra="calculator clouseau converter e_phys ecrire editje edje_viewer elfe elmdentica elsa emote empower enjoy enki epdf envision \
+bin_extra="calculator clouseau converter ephysics ecrire editje edje_viewer elfe elmdentica elsa emote empower enjoy enki epdf envision \
     ephoto espionnage Eterm eve excessive exotic expedite eyelight phonebook rage sticky-notes terminology"
 bin_games="eblock e_cho econcentration eskiss e-type minesweeper ninestime"
 
