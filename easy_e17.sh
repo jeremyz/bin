5ffd2efa6ef85c24236fb5a8ad683d297bd402b6
Jérémy Zurcher <jeremy@asynk.ch>
2012-08-14 23:01:54 +0200

    easy_e17.sh: add shotgun

--- easy_e17.sh
+++ easy_e17.sh
@@ -58,7 +58,7 @@ eweather exebuf execwatch flame forecasts iiirk mail mem moon mpdule net news pe
 rain rmb screenshot skel slideshow snow taskbar tclock uptime weather winlist-ng winselector wlan"
 # itask itask-ng
 bin_proto="ardy calculator converter ecrire econnman editje edje_viewer edvi ekbd elfe elmdentica email emote empower emprint enjoy enki entrance envision \
-epdf eps ephoto equate espionnage eve exactness exalt exalt-client exchange expedite eyelight eyesight phonebook rage sticky-notes vigrid"
+epdf eps ephoto equate espionnage eve exactness exalt exalt-client exchange expedite eyelight eyesight phonebook rage shotgun sticky-notes vigrid"
 # Eterm
 bin_games="eblock e_cho econcentration ehidden_ninjas eskiss e-type"
 
