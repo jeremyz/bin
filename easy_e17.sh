3d341defe2ff4a1e44d3d9685e25458fb3fdba2a
Jérémy Zurcher <jeremy@asynk.ch>
2013-01-04 07:32:20 +0100

    easy_e17.sh: efl swallowed edje and ephysics

--- easy_e17.sh
+++ easy_e17.sh
@@ -49,8 +49,8 @@ cmd_svn_update_conflicts_ask="svn update -r"
 cmake_build_dir="build"
 cmake_options=""
 
-efl_basic="efl e_dbus edje exquisite evas_generic_loaders emotion ethumb emap libeweather elementary expedite"
-efl_extra="azy clouseau efx enlil ensure ephysics esskyuehl etam exactness libast shellementary"
+efl_basic="efl e_dbus exquisite evas_generic_loaders emotion ethumb emap libeweather elementary expedite"
+efl_extra="azy clouseau efx enlil ensure esskyuehl etam exactness libast shellementary"
 efl_bindings="elev8 python-evas python-ecore python-e_dbus python-edje python-emotion python-elementary vala eflxx einaxx eetxx evasxx ecorexx edjexx emotionxx elementaryxx"
 bin_basic="e terminology"
 e_modules_extra="alarm comp-scale cpu deskshow diskio eektool eenvader.fractal empris engage eooorg \
