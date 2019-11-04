0dd22895006cc59cf4f039523ea2ff95ea5f3144
Jérémy Zurcher <jeremy@asynk.ch>
2019-11-04 09:55:29 +0100

    arch-install : fix post chroot

--- arch-install/arch-setup-1.sh
+++ arch-install/arch-setup-1.sh
@@ -3,26 +3,20 @@
 echo "LOCALTIME"
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc --utc
 
 echo "LOCALE"
 locale-gen
 
+echo "PACMAN"
+pacman-db-upgrade
+pacman -Syu
+pacman -S --noconfirm grub efibootmgr mkinitcpio linux linux-firmware vim
+
 echo "MKINITCPIO"
 mkinitcpio -p linux
 
-echo "NETWORK"
-systemctl enable dhcpcd.service
-systemctl start dhcpcd.service
-
-echo "PACMAN"
-pacman-db-upgrade
-
 echo "GRUB"
-pacman -Syu
-pacman -S --noconfirm grub efibootmgr
 #grub-install --target=i386-pc --recheck /dev/sda
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub_uefi --recheck
-
 grub-mkconfig -o /boot/grub/grub.cfg
-
-pacman -S vim
