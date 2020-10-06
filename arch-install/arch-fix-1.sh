71c1550e14b45e74fae5d21ed578fd452f0c3bcc
Jérémy Zurcher <jeremy@asynk.ch>
2020-10-06 09:34:35 +0200

    arch-fix-1.sh : check lib links, call mkinitcpio

--- arch-install/arch-fix-1.sh
+++ arch-install/arch-fix-1.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+echo "LINKS"
+for l in /lib /lib64 /usr/lib64
+do
+    if [ "$(readlink -f $l)" != "/usr/lib" ]
+    then
+        echo "must manually fix : $l     (ln -s /usr/lib $l)"
+        exit 1
+    fi
+done
+
 echo "PACMAN"
 pacman-db-upgrade
 pacman -Syu
@@ -8,3 +18,4 @@ pacman -S --noconfirm grub efibootmgr mkinitcpio linux linux-firmware
 echo "GRUB"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+mkinitcpio -p linux
